4fad9825f7dc2e5fdd778b2ce860b7f1c33452c6
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-07-29 18:46:28 +0900

    Add index action to HomeController

--- lib/jaunty_greeter_web/controllers/home_controller.ex
+++ lib/jaunty_greeter_web/controllers/home_controller.ex
@@ -1,6 +1,10 @@
 defmodule JauntyGreeterWeb.HomeController do
   use JauntyGreeterWeb, :controller
 
+  def index(conn, _params) do
+    render(conn, :index)
+  end
+
   def hello(conn, _params) do
     render(conn, :hello)
   end
