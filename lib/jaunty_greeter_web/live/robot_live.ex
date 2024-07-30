151944f9deb25f00c82610fcb59e244597ef5fe2
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-07-30 10:17:35 +0900

    Add handle_info/2 to RobotLive

--- lib/jaunty_greeter_web/live/robot_live.ex
+++ lib/jaunty_greeter_web/live/robot_live.ex
@@ -7,6 +7,16 @@ defmodule JauntyGreeterWeb.RobotLive do
       |> assign(:counter, 0)
       |> assign(:name, "world")
 
+    Process.send_after(self(), :increment_counter, 1000)
+
     {:ok, socket}
+  end
+
+  def handle_info(:increment_counter, socket) do
+    socket = assign(socket, :counter, socket.assigns.counter + 1)
+
+    Process.send_after(self(), :increment_counter, 1000)
+
+    {:noreply, socket}
   end
 end
