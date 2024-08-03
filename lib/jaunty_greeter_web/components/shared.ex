8d3a1184b88dc62824413a5c5bbe8a57bbb4683a
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-08-03 13:50:27 +0900

    embed_templates "shared/*"

--- lib/jaunty_greeter_web/components/shared.ex
+++ lib/jaunty_greeter_web/components/shared.ex
@@ -1,22 +1,11 @@
 defmodule JauntyGreeterWeb.Shared do
   use JauntyGreeterWeb, :html
 
+  embed_templates "shared/*"
+
+  slot :inner_block, required: true
   attr :label, :string, default: "Label"
   attr :active, :boolean, default: false
 
-  def tab_and_panel(assigns) do
-    ~H"""
-    <input
-      type="radio"
-      name="selected_tab"
-      role="tab"
-      class="tab [--tab-border-color:black] focus:ring-transparent"
-      aria-label={@label}
-      checked={@active}
-    />
-    <div role="tabpanel" class="tab-content p-10">
-      <%= render_slot(@inner_block) %>
-    </div>
-    """
-  end
+  def tab_and_panel(assigns)
 end
