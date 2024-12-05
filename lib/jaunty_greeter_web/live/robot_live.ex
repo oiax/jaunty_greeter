62872006ab1e7d7927e17907d676c866557a7ce1
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-12-06 07:46:57 +0900

    [ch08] RobotLive: assign and embed values

--- lib/jaunty_greeter_web/live/robot_live.ex
+++ lib/jaunty_greeter_web/live/robot_live.ex
@@ -1,3 +1,12 @@
 defmodule JauntyGreeterWeb.RobotLive do
   use JauntyGreeterWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:counter, 0)
+      |> assign(:name, "world")
+
+    {:ok, socket}
+  end
 end
