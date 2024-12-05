925ec1d8a892704b9c0dc8001247dba06b13a09d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-12-05 17:16:19 +0900

    [ch04] Apply layout to the page of HomeController.hello/2

--- lib/jaunty_greeter_web/controllers/home_controller.ex
+++ lib/jaunty_greeter_web/controllers/home_controller.ex
@@ -2,6 +2,6 @@ defmodule JauntyGreeterWeb.HomeController do
   use JauntyGreeterWeb, :controller
 
   def hello(conn, _params) do
-    render(conn, :hello, layout: false)
+    render(conn, :hello)
   end
 end
