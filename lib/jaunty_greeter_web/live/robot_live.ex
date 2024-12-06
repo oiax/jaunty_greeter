e291df29ab93a522bf167f84a6ae0c25ab2f94b5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-12-06 09:00:23 +0900

    [ch10] RobotLive: impl. :increment_counter info message handler

--- lib/jaunty_greeter_web/live/robot_live.ex
+++ lib/jaunty_greeter_web/live/robot_live.ex
@@ -7,6 +7,10 @@ defmodule JauntyGreeterWeb.RobotLive do
       |> assign(:counter, 0)
       |> assign(:name, "world")
 
+    if connected?(socket) do
+      Process.send_after(self(), :increment_counter, 1000)
+    end
+
     {:ok, socket}
   end
 
@@ -23,6 +27,12 @@ defmodule JauntyGreeterWeb.RobotLive do
 
   def handle_event("change_name", %{"name" => name}, socket) do
     socket = assign(socket, :name, name)
+    {:noreply, socket}
+  end
+
+  def handle_info(:increment_counter, socket) do
+    Process.send_after(self(), :increment_counter, 1000)
+    socket = update(socket, :counter, &(&1 + 1))
     {:noreply, socket}
   end
 end
