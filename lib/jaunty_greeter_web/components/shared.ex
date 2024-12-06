9c8c19ae98d1423cac7d5a708b91694fc516b5e5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-12-06 18:45:25 +0900

    [ch16] DroidLive: extract Shared.tab_and_panel/1 to a template

--- lib/jaunty_greeter_web/components/shared.ex
+++ lib/jaunty_greeter_web/components/shared.ex
@@ -1,6 +1,8 @@
 defmodule JauntyGreeterWeb.Shared do
   use JauntyGreeterWeb, :html
 
+  embed_templates "shared/*"
+
   def simple_card(assigns) do
     ~H"""
     <div class="card bg-base-300 w-96 shadow-xl mx-auto my-4">
@@ -15,19 +17,5 @@ defmodule JauntyGreeterWeb.Shared do
   attr :label, :string, default: "Label"
   attr :active, :boolean, default: false
 
-  def tab_and_panel(assigns) do
-    ~H"""
-    <input
-      type="radio"
-      name="selected_tab"
-      role="tab"
-      class="tab [--tab-border-color:black] focus:ring-transparent"
-      aria-label={@label}
-      checked={@active}
-    />
-    <div role="tabpanel" class="tab-content p-10">
-      {render_slot(@inner_block)}
-    </div>
-    """
-  end
+  def tab_and_panel(assigns)
 end
