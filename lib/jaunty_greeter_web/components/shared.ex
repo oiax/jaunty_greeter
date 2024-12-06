e31f89552d79aaf02f7b96cc9b595264bc286692
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-12-06 18:42:19 +0900

    [ch16] DroidLive: refactoring with Shared.tab_and_panel/1

--- lib/jaunty_greeter_web/components/shared.ex
+++ lib/jaunty_greeter_web/components/shared.ex
@@ -10,4 +10,24 @@ defmodule JauntyGreeterWeb.Shared do
     </div>
     """
   end
+
+  slot :inner_block, required: true
+  attr :label, :string, default: "Label"
+  attr :active, :boolean, default: false
+
+  def tab_and_panel(assigns) do
+    ~H"""
+    <input
+      type="radio"
+      name="selected_tab"
+      role="tab"
+      class="tab [--tab-border-color:black] focus:ring-transparent"
+      aria-label={@label}
+      checked={@active}
+    />
+    <div role="tabpanel" class="tab-content p-10">
+      {render_slot(@inner_block)}
+    </div>
+    """
+  end
 end
--- lib/jaunty_greeter_web/live/droid_live.ex
+++ lib/jaunty_greeter_web/live/droid_live.ex
@@ -1,5 +1,6 @@
 defmodule JauntyGreeterWeb.DroidLive do
   use JauntyGreeterWeb, :live_view
+  alias JauntyGreeterWeb.Shared
 
   embed_templates "droid_live/*"
 end
