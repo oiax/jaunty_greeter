8d029db5d04ca04b956243b56b909c10d96a4cc8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-12-06 20:40:07 +0900

    [ch17] Introduce req

--- mix.exs
+++ mix.exs
@@ -52,7 +52,8 @@ defmodule JauntyGreeter.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:req, "~> 0.5.0"}
     ]
   end
 
