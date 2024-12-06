f6b0dda5a6ca2f96c566eb4b84180703ffe197d8
Tsutomu Kuroda <t-kuroda@oiax.jp>
2024-12-06 14:12:13 +0900

    [ch15] DroidLive: FadeOut hook

---
+++ assets/js/hooks/fade_out.js
@@ -0,0 +1,14 @@
+export const FadeOut = {
+  mounted() {
+    const target = this.el
+    const tab = document.querySelector("input[aria-label='Fade out']")
+    const delay = parseInt(target.dataset.delay)
+
+    if (tab && Number.isInteger(delay) && delay > 0) {
+      tab.addEventListener("change", _event => {
+        target.classList.remove("opacity-0")
+        setTimeout(() => target.classList.add("opacity-0"), delay)
+      })
+    }
+  }
+}
